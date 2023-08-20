5b11445da94c61ef420dcedcd2fed789c91bf78b
Rafael Domingo <102312651+RafaDomCas@users.noreply.github.com>
2023-08-20 17:24:36 -0500

    initial setup

---
+++ app/src/main/java/com/example/baubaptechchallenge/BaubapChallengeApp.kt
@@ -0,0 +1,14 @@
+package com.example.baubaptechchallenge
+
+import android.app.Application
+
+class BaubapChallengeApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG){
+
+        }
+    }
+}
